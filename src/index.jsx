f80ed0d01d76039ef8d0407a02bca6da5d75f278
Vincent Chiu <thevincentchiu@gmail.com>
2023-09-23 19:01:16 -0500

    Hosted site on FireBase

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+ 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Begin Firebase Config
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBAqFJ86rtEunGoiy-NZKugTYHNX77oNbE",
+  authDomain: "tutorial-72236.firebaseapp.com",
+  projectId: "tutorial-72236",
+  storageBucket: "tutorial-72236.appspot.com",
+  messagingSenderId: "794370293914",
+  appId: "1:794370293914:web:b65acc1b82a5f645ec1f23"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// End of Firebase Config
 
 root.render(
   <React.StrictMode>
