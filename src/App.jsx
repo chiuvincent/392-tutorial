21926e25f7c9e44c86921bc1aea04e90c8049da3
Vincent Chiu <thevincentchiu@gmail.com>
2023-09-27 10:09:48 -0500

    made initial page

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,16 @@
-import { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 const App = () => {
-  const [count, setCount] = useState(0);
+  // const today = new Date();
+  // const day = today.toLocaleString([], {weekday: 'long'});
+  // const date = today.toLocaleDateString([], {dateStyle: 'long'})
+  const schedule = {
+    title: "CS Courses for 2018-2019"
+  };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount(count => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test hot module replacement (HMR).
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div>
+      <h1>{schedule.title}</h1>
+      
+      {/* <p>Today is {day}, {date}.</p> */}
     </div>
   );
 };
