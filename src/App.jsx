a0d889937e6961f2fae0a36c4b8a49815d69a077
Vincent Chiu <thevincentchiu@gmail.com>
2023-09-29 22:12:52 -0500

    Show courses

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,42 @@
+import Banner from './components/Banner';
+import CourseList from './components/CourseList';
+
 const App = () => {
-  // const today = new Date();
-  // const day = today.toLocaleString([], {weekday: 'long'});
-  // const date = today.toLocaleDateString([], {dateStyle: 'long'})
+
   const schedule = {
-    title: "CS Courses for 2018-2019"
+    "title": "CS Courses for MMXXII-MMXXIII",
+    "courses": {
+      "F101" : {
+        "term": "Fall",
+        "number": "101",
+        "meets" : "MWF 11:00-11:50",
+        "title" : "Computer Science: Concepts, Philosophy, and Connections"
+      },
+      "F110" : {
+        "term": "Fall",
+        "number": "110",
+        "meets" : "MWF 10:00-10:50",
+        "title" : "Intro Programming for non-majors"
+      },
+      "S313" : {
+        "term": "Spring",
+        "number": "313",
+        "meets" : "TuTh 15:30-16:50",
+        "title" : "Tangible Interaction Design and Learning"
+      },
+      "S314" : {
+        "term": "Spring",
+        "number": "314",
+        "meets" : "TuTh 9:30-10:50",
+        "title" : "Tech & Human Interaction"
+      }
+    }
   };
 
   return (
     <div>
-      <h1>{schedule.title}</h1>
-      
-      {/* <p>Today is {day}, {date}.</p> */}
+      <Banner title={schedule.title}/>
+      <CourseList courses={schedule.courses}/>
     </div>
   );
 };
---
+++ src/components/CourseList.jsx
@@ -0,0 +1,11 @@
+const CourseList = ({courses}) => (
+    <div>
+        { Object.entries(courses).map(([id, course]) => 
+            <div key={id}> 
+                {course.term} CS {course.number}: {course.title}
+            </div>) }
+    </div>
+    
+);
+  
+export default CourseList;
